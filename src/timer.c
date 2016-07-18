ab6f7d1a2a0629903116ccba60fa3f0a89050da5
GandalfDaGreat <jackacase@gmail.com>
2016-07-18 14:00:39 -0400

    working on timer

--- src/timer.c
+++ src/timer.c
@@ -1,3 +1,38 @@
 #include <pebble.h>
 #include "timer.h"
 
+void set_timer() {
+	timer_seconds = 60 * round_count--; // set the time and decrement the round count
+}
+
+void start_timer() {
+	timer_running = true;
+}
+
+void stop_timer() {
+	timer_running = false;
+}
+
+// refresh the time text
+void update_timer_text() {
+	int minutes = timer_seconds / 60;
+	int seconds = timer_seconds % 60;
+	static char time[6];
+	
+	snprintf(time, 6, "%02d:%02d", minutes, seconds);
+	
+	text_layer_set_text(timer_text, time);
+}
+
+// if the timer is running decrement, if the timer runs out -1 is returned
+int time_tick() {
+	if(timer_running) {
+		timer_seconds--;
+		update_timer_text();
+		if(timer_seconds <= 0) {
+			stop_timer();
+			return -1; // the time has run out	
+		}
+	}
+	return 0;
+}
--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,10 @@
 #pragma once
 
 int timer_seconds;
+bool timer_running;
 
+Window *timer_window;
+GBitmap *timer_background_bitmap;
+TextLayer *timer_text;
+TextLayer *round_text;
+TextLayer *hostage_text;
